4514d8b7d7a987be776bf5dead4d60b706c07dcc
arxci <garretthumbert9@gmail.com>
2025-12-13 14:01:34 -0600

    Added dispaly font to body

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,12 +3,14 @@ import type { Metadata } from "next";
 import { SiteHeader } from "./_components/layout/site-header";
 import { SiteFooter } from "./_components/layout/site-footer";
 
+import { Providers } from "./_helpers/providers";
+
 import { siteConfig } from "@/config/site";
-import { fontSerif } from "@/lib/fonts";
+
+import { fontDisplay, fontSerif } from "@/lib/fonts";
 import { cn } from "@/lib/utils";
 
 import "@/styles/globals.css";
-import { Providers } from "./_helpers/providers";
 
 export const metadata: Metadata = {
   title: siteConfig.name,
@@ -22,7 +24,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={cn("antialiased", fontSerif.className)}>
+      <body
+        className={cn("antialiased", fontDisplay.variable, fontSerif.className)}
+      >
         <Providers>
           <div className="grid grid-rows-[auto_1fr_auto] min-h-screen">
             <SiteHeader />
