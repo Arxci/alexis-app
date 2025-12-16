3ab0aa6024fb9ddf5f050914bc4d1f0e93236bfb
arxci <garretthumbert9@gmail.com>
2025-12-15 21:29:41 -0600

    Added sanity route

---
+++ sanity.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from "sanity";
+import { structureTool } from "sanity/structure";
+
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
+
+export default defineConfig({
+  name: "default",
+  title: "My Sanity Studio",
+
+  projectId,
+  dataset,
+
+  basePath: "/studio",
+
+  plugins: [structureTool()],
+
+  schema: {
+    types: [],
+  },
+});
