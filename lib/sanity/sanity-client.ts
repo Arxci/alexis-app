0a53866b662837f5eafc90fb67255bd97a56ce05
arxci <garretthumbert9@gmail.com>
2025-12-15 20:54:32 -0600

    Remove redundant if checks in sanity client

--- lib/sanity/sanity-client.ts
+++ lib/sanity/sanity-client.ts
@@ -1,20 +1,6 @@
 import { createClient } from "next-sanity";
 import { env } from "../env";
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
-const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION;
-
-if (!projectId) {
-  throw new Error("Missing NEXT_PUBLIC_SANITY_PROJECT_ID");
-}
-if (!dataset) {
-  throw new Error("Missing NEXT_PUBLIC_SANITY_DATASET");
-}
-if (!apiVersion) {
-  throw new Error("Missing NEXT_PUBLIC_SANITY_API_VERSION");
-}
-
 export const client = createClient({
   projectId: env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   dataset: env.NEXT_PUBLIC_SANITY_DATASET,
